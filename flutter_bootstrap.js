052bc40a46cb99a47bd70020600708e25faff1fa
tanoguchi28-cpu <tanoguchi28@gmail.com>
2026-09-03 23:12:15 +0900

    Fix: auto-create profile with org_id on signup and team join

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4049701310" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "491571370" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
